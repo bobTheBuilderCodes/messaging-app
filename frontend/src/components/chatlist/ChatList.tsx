145349717c73e44474cd6a07e19e4b34c238677a
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2023-03-19 15:11:10 +0000

    Chatbox UI

---
+++ frontend/src/components/chatlist/ChatList.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const ChatList = () => {
+  return (
+    <div className='chatListContainer'>
+      <p>Philomena Potinge</p>
+      <small>This is the last message</small>
+    </div>
+  )
+}
+
+export default ChatList
